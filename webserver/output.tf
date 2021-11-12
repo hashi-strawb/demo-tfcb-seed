5f03a8ad914ae0580281a992d72db8ab148a79a2
Lucy Davinhart <lucy@lucymhdavies.com>
2021-11-12 16:44:58 +0000

    Item 0 from webserver module lists

--- webserver/output.tf
+++ webserver/output.tf
@@ -1,25 +1,15 @@
-/*
 output "london_ec2_connect_url" {
-  value = var.region == "london" ? module.webserver-london.ec2_connect_url : "NONE"
+  value = var.region == "london" ? module.webserver-london[0].ec2_connect_url : "NONE"
 }
 
 output "london_web_server_url" {
-  value = var.region == "london" ? module.webserver-london.web_server_url : "NONE"
+  value = var.region == "london" ? module.webserver-london[0].web_server_url : "NONE"
 }
 
 output "ireland_ec2_connect_url" {
-  value = var.region == "ireland" ? module.webserver-ireland.ec2_connect_url : "NONE"
+  value = var.region == "ireland" ? module.webserver-ireland[0].ec2_connect_url : "NONE"
 }
 
 output "ireland_web_server_url" {
-  value = var.region == "ireland" ? module.webserver-ireland.web_server_url : "NONE"
-}
-*/
-
-output "ireland_ec2_connect_url" {
-  value = module.webserver-ireland.ec2_connect_url
-}
-
-output "ireland_web_server_url" {
-  value = module.webserver-ireland.web_server_url
+  value = var.region == "ireland" ? module.webserver-ireland[0].web_server_url : "NONE"
 }
