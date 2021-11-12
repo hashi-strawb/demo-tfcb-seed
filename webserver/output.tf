ae29949145373b36f65513ae92a0d5b14f3817f5
Lucy Davinhart <lucy@lucymhdavies.com>
2021-11-12 16:43:38 +0000

    force ireland output vars for now to test

--- webserver/output.tf
+++ webserver/output.tf
@@ -1,3 +1,4 @@
+/*
 output "london_ec2_connect_url" {
   value = var.region == "london" ? module.webserver-london.ec2_connect_url : "NONE"
 }
@@ -12,4 +13,13 @@ output "ireland_ec2_connect_url" {
 
 output "ireland_web_server_url" {
   value = var.region == "ireland" ? module.webserver-ireland.web_server_url : "NONE"
+}
+*/
+
+output "ireland_ec2_connect_url" {
+  value = module.webserver-ireland.ec2_connect_url
+}
+
+output "ireland_web_server_url" {
+  value = module.webserver-ireland.web_server_url
 }
